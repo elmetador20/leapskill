3d5955cd408459fb1fba06a952837980d8fe75c4
sharique ahmad <ahmedsharique250@gmail.com>
2025-07-31 17:37:22 +0530

    added landing page

--- components/header.jsx
+++ components/header.jsx
@@ -26,11 +26,11 @@ export default function Header() {
       <nav className="container mx-2 px-4 h-16 flex items-center justify-between">
         <Link href="/">
           <Image
-            src={"/logo .png"}
+            src={"/leap.png"}
             alt="Sensai Logo"
-            width={200}
-            height={60}
-            className="h-12 py-1 w-auto object-contain"
+            width={300}
+            height={100}
+            className="h-16 py-1 w-auto object-contain"
           />
         </Link>
 
